76d08baccf9024e344dfac14e4077beb6ff0e9ae
Ebenezer Ankrah <ebenankrah@gmail.com>
2023-03-21 09:58:04 -0400

    Build user & message resource and add actual message from table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Ebenezer", password: "password123")
+User.create(username: "Bob", password: "password123")
+User.create(username: "Joe", password: "password123")
+User.create(username: "Sam", password: "password123")
+User.create(username: "Janet", password: "password123")
+
+Message.create(body: "Hello everyone", user_id: 3)
+Message.create(body: "Bye bye message", user_id: 1)
+Message.create(body: "I'm here everyone", user_id: 2)
+Message.create(body: "Welcome everyone", user_id: 4)
+Message.create(body: "How is everyone", user_id: 5)
